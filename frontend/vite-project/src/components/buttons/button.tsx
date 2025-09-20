f0e105ac0ac11a8e6ffb1387fff425352c611712
Shreyash <shreyashiitr22@gmail.com>
2025-09-20 21:11:48 +0530

    adding components of the ui

---
+++ frontend/vite-project/src/components/buttons/button.tsx
@@ -0,0 +1,26 @@
+export function RecoveryButton(){
+    return <div className="flex flex-row">
+        <button className="m-2 pl-5 pr-5">
+           <div className="flex flex-row">
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+</svg>
+
+            <div className="font-black pl-4 pr-4">
+                Recovery Setup
+            </div>
+
+           </div>
+   
+        </button>
+<button className="m-2 pl-5 pr-5">
+<div className="flex flex-row ">
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
+</svg>
+
+<div className="font-black pl-4 pr-4"> Wallet Manager</div>
+</div></button>
+    </div>
+}
---
+++ frontend/vite-project/src/components/header/header.tsx
@@ -0,0 +1,6 @@
+export function header(){
+    return <div className="flex flex-col">
+        <div className="font-bold">Vessel</div>
+        <div className="font-light">Secure Multi-Currency Wallet</div>
+    </div>
+}
