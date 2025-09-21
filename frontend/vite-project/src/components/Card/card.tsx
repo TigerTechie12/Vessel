3dbdcbf334bd67d17f1e282168039dc57126768e
Shreyash <shreyashiitr22@gmail.com>
2025-09-21 16:56:15 +0530

    added phrase generator

--- frontend/vite-project/src/components/Card/card.tsx
+++ frontend/vite-project/src/components/Card/card.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+    const [name,setName]=useState(" Generate Recovery Phrase")
+    const [loader,setLoader]=useState(true)
 export function Card(){
+
     return <div className="flex flex-col bg-amber-50 rounded-sm">
         <div className="flex flex-row">
                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -12,9 +16,12 @@ export function Card(){
 <div className="bg-amber-200">
     <div className="font-bold text-amber-800">Security Notice</div><div className="text-amber-800">Write down your recovery phrase and store it in a safe place.Anyone with access to this phrase can control your wallets.</div>
 </div>
-   <button className="bg-amber-500">   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+   <button onClick={PhraseGenerate} className="bg-amber-500">   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
 </svg>
-    Generate Recovery Phrase</button>
+   {name}</button>
     </div>
+}
+function PhraseGenerate(){
+axios.get()
 }
