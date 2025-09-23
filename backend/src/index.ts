3dd2dc62c51769939e57d28a64a87ecf4a1fef78
Shreyash <shreyashiitr22@gmail.com>
2025-09-23 23:16:59 +0530

    initialize database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,22 @@
 import express from 'express';
 import nacl from 'tweetnacl';
-import { generateMnemonic } from 'bip39';
+import { generateMnemonic,mnemonicToSeedSync } from 'bip39';
 import cors from 'cors';
 const app=express()  
 
 app.use(cors())
 
-app.get('/',(req,res)=>{
+app.get('/generateMnemonic',(req,res)=>{
 const mnemonic=generateMnemonic()
+
 res.json({mnemonic})
 
+
 })
+app.get('/addWallet',(req,res)=>{
+const seed=mnemonicToSeedSync(mnemonic)
+
+})
+
+
 app.listen(3000)
