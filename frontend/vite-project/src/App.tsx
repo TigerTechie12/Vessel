82899477d0f86618134b75f40a93c4edf25469fe
Shreyash <shreyashiitr22@gmail.com>
2025-10-05 23:21:26 +0530

    added routing

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Dashboard from './pages/dashboard'
+import {Wallets} from './pages/wallet'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Dashboard></Dashboard>}></Route>
+      <Route path='/wallet' element={<Wallets></Wallets>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
